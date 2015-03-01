f657e732d57752d8db4f099690dcf6b44876ed53
regzand <regzand@gmail.com>
2015-03-01 02:15:53 +0100

    Add sync loading, add comments

--- rTranslation.js
+++ rTranslation.js
@@ -1,54 +1,85 @@
+// node.js modules
 var fs		= require('fs');
 
+// rTranslation class
 function rTranslation(options){
-	var _this = this;
+	var lang = this;
 
-	this.options = options;
-	this.translation = {};
+	lang.options = options;
+	lang.translation = {};
 
-	this.loadTranslationFromFile = function loadTranslationFromFile(file, callback) {
+	// load translation form file
+	lang.loadTranslationFromFile = function loadTranslationFromFile(file, callback){
 		fs.readFile(file, function(err, data){
+			// return IO errors
 			if(err) return callback(err);
 
+			// catch JSON parse errors
 			try {
 				data = JSON.parse(data);
-				_this.loadTranslation(data.language, data.translation, callback);
+
+				// load translation from loaded object
+				lang.loadTranslation(data.language, data.translation, callback);
 			}catch(err){
 				return callback(err);
 			}
 		});
 	};
 
-	this.loadTranslation = function loadTranslation(lang, obj, callback) {
-		if(!_this.translation[lang])_this.translation[lang] = {};
+	// load translation form file (sync)
+	lang.loadTranslationFromFileSync = function loadTranslationFromFileSync(file){
+		// load file
+		var data = fs.readFileSync(file);
 
-		for(var attr in obj){
-			_this.translation[lang][attr] = obj[attr];
-		}
+		// parse data
+		data = JSON.parse(data);
 
-		return callback(null);
+		// load translation from loaded object
+		lang.loadTranslation(data.language, data.translation);
 	};
 
-	this.t = this.getTranslation = function getTranslation(key, lang){
-		lang = lang || _this.options.language;
+	// load translation from js object
+	lang.loadTranslation = function loadTranslation(language, obj, callback){
+		// create language if not exists
+		if(!lang.translation[language])
+			lang.translation[language] = {};
 
+		// overwrite with new translation
+		for(var attr in obj){
+			lang.translation[language][attr] = obj[attr];
+		}
+		
+		// callback if needed
+		if(callback) callback(null);
+	};
+
+	// return translation
+	lang.t = lang.getTranslation = function getTranslation(key, language){
+		// get default language if needed
+		language = language || lang.options.language;
+
+		// catch wrong keys
 		try{
 			var split = key.split(".");
-			var last = _this.translation[lang];
+			var last = lang.translation[language];
 			for(var i = 0; i<split.length; i++){
 				last = last[split[i]];
 			}
 
+			// catch keys targeting to something unintresting
 			if(typeof last === 'object' || typeof last === 'undefined')
-				throw "this key points to object or undefined";
+				throw new Error("this key points to object or undefined");	
 
+			// return rtanslation parsed to string
 			return ''+last;
 		}catch(e){
-			return _this.options.wrong || key;
+			// return wrong identificator
+			return lang.options.wrong || key;
 		}
 	}
 }
 
+// node.js module export
 module.exports = function(options){
 	options = options || {};
 
